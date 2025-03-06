31c4489099b4e31661de23b761474501d39220ad
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-06 15:55:59 +0530

    fix issue on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -31,7 +31,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={ () => fetchData()}>
+        <button onClick={ () => {
+          fetchData()
+          setCount((count) => count + 1)
+        }}>
           count is {count}
         </button>
         <p>
