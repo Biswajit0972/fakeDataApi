8e3af650edd50dfe95d7917e51d85e40b1f74755
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-12 10:37:50 +0530

    fix issue

--- frontend/src/query/hooks/query.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query"
-import { getAllData } from "../../queryFunctions"
-
-export const useGetContentHook = ({url}: {url:string}) => {
-    return useQuery({
-        queryKey: ["getAllContent"],
-        queryFn: () => getAllData(url)
-    })
-}
